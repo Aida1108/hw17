d524212dbaa5f663c0c148ddcb7a479b79de7930
Aida1108 <mi.aida@yandex.ru>
2022-12-19 20:24:45 +0500

    Сделано ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Задание 1
+        System.out.println("Задание 1");
+        String fistName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+
+        String fullName = lastName + " " + fistName + " " + middleName;
+        System.out.println("ФИО сотрудника - " + fullName);
+
+        //Задание 2
+        System.out.println("Задание 2");
+        String name = "Ivanov Ivan Ivanovich";
+        String upperFullName = name.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета - " + upperFullName);
+
+        //Задание 3
+        System.out.println("Задание 3");
+        String name1 = "Иванов Семён Семёныч";
+        String fixedFullName = name1.replace("ё", "е");
+        System.out.println("Данные ФИО сотрудника - " + fixedFullName);
+
     }
 }
